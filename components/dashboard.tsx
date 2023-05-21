98c5eac29a9c41073ef5e06ff6271a7c91cbe11d
Akshat <akshatpyrotech@gmail.com>
2023-05-21 22:57:31 +0530

    Update dashboard.tsx

--- components/dashboard.tsx
+++ components/dashboard.tsx
@@ -59,7 +59,7 @@ const Dashboard = ({ datarec }: { datarec: any }) => {
 		labels: chartLabels,
 		datasets: [
 			{
-				label: "Data",
+				label: "Economy",
 				data: chartDataPoints,
 				backgroundColor: "rgba(75,192,192,0.2)",
 				borderColor: "rgba(75,192,192,1)",
